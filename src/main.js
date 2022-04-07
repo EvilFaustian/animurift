99037c04bb8db7fe3c0077b58d77c0c6d54fd22b
EvilFaustian <101469392+EvilFaustian@users.noreply.github.com>
2022-04-07 14:25:32 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,12 +18,7 @@ import 'firebase/firestore';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDColARAihPdWZjMcd0nNgpwKzcm6Uw64o",
-  authDomain: "animurift.firebaseapp.com",
-  projectId: "animurift",
-  storageBucket: "animurift.appspot.com",
-  messagingSenderId: "168918385522",
-  appId: "1:168918385522:web:456f0eafc7314d798113eb"
+//Firebase Config
 };
 
 // Initialize Firebase
